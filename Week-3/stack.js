9da45e396299d1163294f7765a77e54c204bc3ba
PERFICIENT\p.palandurkar <p.palandurkar@perficient.com>
2022-05-31 10:10:36 +0530

    Refactored createStack function

---
+++ Week-3/stack.js
@@ -0,0 +1,21 @@
+function createStack()
+{
+    const items = [];
+    return {
+        push(item) {
+            items.push(item)
+        },
+        pop() {
+            return items.pop()
+        }
+    }
+    
+         
+}
+let p = createStack()
+
+p.push(4)
+p.push(5)
+p.pop() // 5
+console.log(p.items) // undefined
+
